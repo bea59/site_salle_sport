f1f2e95abf87dec888101f53129203d24c2258ef
bea <issambakkali249@gmail.com>
2023-12-07 12:34:49 +0100

    création des routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('acceuil');
+});
+Route::get('/', function () {
+    return view('cours');
+});
+Route::get('/', function () {
+    return view('tarif');
+});
+Route::get('/', function () {
+    return view('contact');
+});
+Route::get('/', function () {
+    return view('mentions-legales');
 });
