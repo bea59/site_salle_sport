af2eb5e0bed33fc8b1e3a8d2b698426c3527a64e
bea <issambakkali249@gmail.com>
2023-12-07 22:58:13 +0100

    Ajout du contenu HTML et CSS dans les 5 pages correspondantes; accueil, cours, tarifs, contact et mentions_legales

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -7,5 +7,5 @@
 <section class="content">
     <img src="images\IMG-20231207-WA0003.jpg" alt="image de sport">
     <img src="images\IMG-20231207-WA0005.jpg" alt="image de fitness">
+    <h2>Lorem ipsum</h2>
 @endsection 
-    
